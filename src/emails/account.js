5e8ae740c997d8e02beeaae352006a8de2007a44
Florent VICENTE <vicentegroupe@gmail.com>
2019-06-11 12:07:08 +0200

    tentative de debug globale

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,28 +1,26 @@
-const sgMail = require("@sendgrid/mail");
+const sgMail = require('@sendgrid/mail')
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
-  sgMail.send({
-    to: email,
-    from: "vicentegroupe@gmail.com",
-    subject: "Youlilol",
-    text: `Welcome to the app, ${name}. Let me know blabla`,
-    html: ""
-  });
-};
+    sgMail.send({
+        to: email,
+        from: 'vicentegroupe@gmail.com',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
 
-const sendGoodbyEmail = (email, name) => {
-  sgMail.send({
-    to: email,
-    from: "vicentegroupe@gmail.com",
-    subject: "Quel dommage!",
-    text: `Vous partez dejà ${name} ?`,
-    html: ""
-  });
-};
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'vicentegroupe@gmail.com',
+        subject: 'Sorry to see you go!',
+        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+    })
+}
 
 module.exports = {
-  sendWelcomeEmail,
-  sendGoodbyEmail
-};
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
